120481796dd07b2391d93fa9560bb5cd11f3000e
Michelle Schlicher <67785064+schlicherM@users.noreply.github.com>
2024-08-08 15:43:06 +0200

    fix questionnaire scale

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -12,30 +12,30 @@ def preprocess_data(data: pd.DataFrame) -> pd.DataFrame:
     - pd.DataFrame: The preprocessed data.
     """
     column_rename_dict = {
-        'C101_01': 'MDBF_Awake', # 1 (Sehr müde) - 101 (Sehr wach), -9 not answered
-        'C101_02': 'MDBF_Satisfied', # 1 (Sehr unzufrieden) - 101 (Sehr zufrieden), -9 not answered
-        'C101_03': 'MDBF_Calm', # 1 (Sehr unruhig) - 101 (Sehr ruhig), -9 not answered
-        'C101_04': 'MDBF_Energy', # 1 (Sehr ernergielos) - 101 (Sehr energiegeladen), -9 not answered
-        'C101_05': 'MDBF_Unwell', # 1 (Sehr unwohl) - 101 (Sehr wohl), -9 not answered
-        'C101_06': 'MDBF_Relaxed', # 1 (Sehr entspannt) - 101 (Sehr angespannt), -9 not answered
-        'C201_01': 'PSS4_Control', # 1 (Nie) - 101 (Sehr oft), -9 not answered
-        'C201_02': 'PSS4_Stress', # 1 (Nie) - 101 (Sehr oft), -9 not answered
-        'C201_03': 'PSS4_Taks', # 1 (Nie) - 101 (Sehr oft), -9 not answered
-        'C201_04': 'PSS4_Obstacles', # 1 (Nie) - 101 (Sehr oft), -9 not answered
-        'C301_01': 'SSCCS_Giveup', #  1 (Stimme überhaupt nicht zu) - 7 (Stimme völlig zu), -9 not answered
-        'C301_02': 'SSCCS_Resist',  # 1 (Stimme überhaupt nicht zu) - 7 (Stimme völlig zu), -9 not answered
-        'C401': 'Topics_None', # negative or number selected
-        'C401_01': 'Topics_Movement', # 1 (not selected) or 2 (selected)
-        'C401_02': 'Topics_Family', # 1 or 2
-        'C401_03': 'Topics_Food', # 1 or 2
-        'C401_04': 'Topics_Friends', # 1 or 2
-        'C401_05': 'Topics_Religion', # 1 or 2
-        'C401_06': 'Topics_Health', # 1 or 2
-        'C401_07': 'Topics_Love', # 1 or 2
-        'C401_08': 'Topics_Recreation', # 1 or 2
-        'C401_09': 'Topics_School', # 1 or 2
-        'C401_10': 'Topcis_Sleep', # 1 or 2
-        'C401_11': 'Topics_Work' # 1 or 2
+        'MB01_01': 'MDBF_Awake', # 1 (Sehr müde) - 7 (Sehr wach), -9 not answered
+        'MB01_02': 'MDBF_Satisfied', # 1 (Sehr unzufrieden) - 7 (Sehr zufrieden), -9 not answered
+        'MB01_03': 'MDBF_Calm', # 1 (Sehr unruhig) - 7 (Sehr ruhig), -9 not answered
+        'MB01_04': 'MDBF_Energy', # 1 (Sehr ernergielos) - 7 (Sehr energiegeladen), -9 not answered
+        'MB01_05': 'MDBF_Unwell', # 1 (Sehr unwohl) - 7 (Sehr wohl), -9 not answered
+        'MB01_06': 'MDBF_Relaxed', # 1 (Sehr entspannt) - 7 (Sehr angespannt), -9 not answered
+        'PS01_01': 'PSS4_Control', # 1 (Nie) - 7 (Sehr oft), -9 not answered
+        'PS01_02': 'PSS4_Stress', # 1 (Nie) - 5 (Sehr oft), -9 not answered
+        'PS01_03': 'PSS4_Taks', # 1 (Nie) - 5 (Sehr oft), -9 not answered
+        'PS01_04': 'PSS4_Obstacles', # 1 (Nie) - 5 (Sehr oft), -9 not answered
+        'SC01_01': 'SSCCS_Giveup', #  1 (Stimme überhaupt nicht zu) - 7 (Stimme völlig zu), -9 not answered
+        'SC01_02': 'SSCCS_Resist',  # 1 (Stimme überhaupt nicht zu) - 7 (Stimme völlig zu), -9 not answered
+        'TP01': 'Topics_None', # negative or number selected
+        'TP01_01': 'Topics_Movement', # 1 (not selected) or 2 (selected)
+        'TP01_02': 'Topics_Family', # 1 or 2
+        'TP01_03': 'Topics_Food', # 1 or 2
+        'TP01_04': 'Topics_Friends', # 1 or 2
+        'TP01_05': 'Topics_Religion', # 1 or 2
+        'TP01_06': 'Topics_Health', # 1 or 2
+        'TP01_07': 'Topics_Love', # 1 or 2
+        'TP01_08': 'Topics_Recreation', # 1 or 2
+        'TP01_09': 'Topics_School', # 1 or 2
+        'TP01_10': 'Topcis_Sleep', # 1 or 2
+        'TP01_11': 'Topics_Work' # 1 or 2
     }
 
     # Rename columns
